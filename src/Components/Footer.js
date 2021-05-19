67b583d0938a4f159a65bd7f319e28cca1c749b8
Muhammad Waqas Rizvi <waqashafiz90@gmail.com>
2021-05-19 19:09:51 +0500

    Completed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,8 +2,48 @@ import React from 'react'
 
 export const Footer = () => {
     return (
-        <div>
-            <h1>This is Footer</h1>
+        <>
+        <div className="footer">
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-3 mt-3">
+                        <ul>
+                            <li className="mb-2" style={{color: "#002f34", fontWeight: "bold", listStyle: "none"}}>POPULAR CATEGORIES</li>
+                            <li className="foot"><a href="#">Cars</a></li>
+                            <li className="foot"><a href="#">Flats for rent</a></li>
+                            <li className="foot"><a href="#">Jobs</a></li>
+                            <li className="foot"><a href="#">Mobile Phones</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-sm-3 mt-3">
+                        <ul>
+                            <li className="mb-2" style={{color: "#002f34", fontWeight: "bold", listStyle: "none"}}>TRENDING SEARCHES</li>
+                            <li className="foot"><a href="#">Bikes</a></li>
+                            <li className="foot"><a href="#">Watches</a></li>
+                            <li className="foot"><a href="#">Books</a></li>
+                            <li className="foot"><a href="#">Dogs</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-sm-3 mt-3">
+                        <ul>
+                            <li className="mb-2" style={{color: "#002f34", fontWeight: "bold", listStyle: "none"}}>ABOUT US</li>
+                            <li className="foot"><a href="#">About OLX Group</a></li>
+                            <li className="foot"><a href="#">OLX Blog</a></li>
+                            <li className="foot"><a href="#">Contact Us</a></li>
+                            <li className="foot"><a href="#">OLX for Businesses</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-sm-3 mt-3">
+                        <ul>
+                            <li className="mb-2" style={{color: "#002f34", fontWeight: "bold", listStyle: "none"}}>OLX</li>
+                            <li className="foot"><a href="#">Help</a></li>
+                            <li className="foot"><a href="#">Sitemap</a></li>
+                            <li className="foot"><a href="#">Legal & Privacy information</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
+        </>
     )
 }
--- src/Components/Footer2.js
+++ src/Components/Footer2.js
@@ -2,8 +2,24 @@ import React from 'react'
 
 export const Footer2 = () => {
     return (
-        <div>
-            <h1>This is Footer2</h1>
-        </div>
+        <>
+            <div className="foot2">
+                <div className="foot2text waqas">
+                    <ul>
+                        <li>
+                            <span style={{ fontSize: "14px", fontWeight: "bold" }}>Other Countries</span>
+                            <span><a href="#" style={{ textDecoration: "none", color: "white", fontSize: "14px", marginLeft: "15px" }}>India-South Africa-Indonesia</a></span>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <span style={{ color: "white", fontSize: "14px", fontWeight: "bold" }}>Free Classifieds in Pakistan
+                        <span style={{ fontWeight: "normal" }}>&nbsp;&nbsp;. © 2006-2021 OLX</span>
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </>
     )
 }
